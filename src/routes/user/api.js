f2ee41bf3e9c8153eb2d181038dcb7676bf3a771
Jeffrey <ref6318@gmail.com>
2021-10-08 10:36:16 +0900

    add: 로그인 회원가입 라우터 생성

---
+++ src/routes/user/api.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const express = require('express');
+const router = express.Router();
+
+router.post('/login');
+router.post('/signup');
+
+module.exports = router;
---
+++ src/routes/user/render.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const express = require('express');
+const router = express.Router();
+
+router.get('/login', function (req, res) {
+
+});
+
+router.get('/signup', function (req, res) {
+
+});
+
+module.exports = router;
