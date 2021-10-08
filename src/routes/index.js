eb7be753e7d1f254648072bb441c746536020a4c
Jeffrey <ref6318@gmail.com>
2021-10-08 10:36:00 +0900

    add: routes index

---
+++ src/routes/index.js
@@ -0,0 +1,14 @@
+// router
+
+const userAPI = require('.//user/api');
+const userRender = require('.//user/render');
+
+module.exports = (app) => {
+    app.get('/', (req, res) => {
+        res.render('../views/main/main');
+    });
+
+    // user
+    app.use('/api/user', userAPI);
+    app.use('/user', userRender);
+}
