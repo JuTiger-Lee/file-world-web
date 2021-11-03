10d035fe9e3af416c15419c432ec2d0ee2c8b2ba
Jeffrey <ref6318@gmail.com>
2021-11-03 09:40:59 +0900

    fix: db password dotenv 접근

--- src/config/db.js
+++ src/config/db.js
@@ -7,7 +7,7 @@ module.exports = {
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
     database: process.env.DB_DATABASE,
-    password: process.env.DB_PASSOWRD,
+    password: process.env.DB_PASSWORD,
     port: process.env.DB_PORT,
     dateStrings: ['DATE', 'DATETIME'],
     timezone: '+09:00',
