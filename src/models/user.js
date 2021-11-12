8956ead05b3e649b98761e6b9ebb3bfda1078f16
Jeffrey <ref6318@gmail.com>
2021-11-12 09:57:43 +0900

    fix; nickname id 중복 체크 코드 추가

--- src/models/user.js
+++ src/models/user.js
@@ -1,26 +1,28 @@
 const db = require('./db');
 
-async function findUser(params) {
+async function userFindID(params) {
   const sql = 'SELECT * FROM user WHERE ui_id = ?';
 
   return db.query(sql, params);
 }
 
-async function createUser(params) {
+async function userFindNickName(params) {
+  const sql = 'SELECT * FROM user WHERE ui_nickname = ?';
+
+  return db.query(sql, params);
+}
+
+async function userCreate(params) {
   const sql =
-    'INSERT INTO user(ui_email, ui_name, ui_id, ui_password, ui_email_status, create_datetime, update_datetime)' +
+    'INSERT INTO user(ui_email, ui_name, ui_id, ui_password,' +
+    'ui_email_status, create_datetime, update_datetime)' +
     'VALUES(?, ?, ?, ?, ?, now(), now())';
 
   return db.query(sql, params);
 }
 
-async function updateUser() {}
-
-async function deleteUser() {}
-
 module.exports = {
-  findUser,
-  updateUser,
-  createUser,
-  deleteUser,
+  userFindID,
+  userFindNickName,
+  userCreate,
 };
