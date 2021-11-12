0367cc6dd8267e6f7b4e93e2c041467c30491b8b
Jeffrey <ref6318@gmail.com>
2021-11-12 09:56:56 +0900

    fix: 404 render 및 500 error handleing 추가

--- src/middleware/error.js
+++ src/middleware/error.js
@@ -1,25 +1,28 @@
 module.exports = app => {
   // 404 error
   app.use((req, res) => {
-    const result = {
-      status: 404,
-      message: '404 Not Found',
-      error: {},
-      data: {},
-    };
+    // const result = {
+    //   status: 404,
+    //   message: '404 Not Found',
+    //   error: {},
+    //   data: {},
+    // };
+    // return res.status(404).json(result);
 
-    return res.status(404).json(result);
+    res.render('../views/common/notFound');
   });
 
   app.use((err, req, res, next) => {
+    console.log('err-name', err.name);
+
     const result = {
-      status: err.status || 500,
+      code: err.code || 500,
       message: err.message || '500 server Error',
-      error: err.status && err.message ? {} : err,
-      data: {},
     };
 
-    res.status(err.status || 500);
+    if (err.err || err) result.error = err.err || err || {};
+
+    res.status(err.httpStatus || 500);
 
     return res.json(result);
   });
