d56f2d304779e06864b55a4a6dffc725361291f8
Jeffrey <ref6318@gmail.com>
2021-11-12 09:46:17 +0900

    fix: error status handle 코드 추가

--- public/js/ajax.js
+++ public/js/ajax.js
@@ -7,17 +7,21 @@
  * @param {Array || Object} params
  * @returns
  */
-
 async function reqAjax(url, method, params) {
-  try {
-    // token send
-    const config = {
-      headers: {
-        Authorization: `bearer ${localStorage.getItem('user')}`,
-      },
-    };
+  const screen = document.querySelector('.screen');
+  const bearerToken = `bearer ${localStorage.getItem('token')}`;
 
-    let getReqResults = {};
+  // token send
+  const config = {
+    headers: {
+      Authorization: bearerToken,
+    },
+  };
+
+  let getReqResults = {};
+
+  try {
+    screen.style.display = 'block';
 
     if (method === 'post' || method === 'put') {
       getReqResults = await axios[method](url, params, config);
@@ -27,11 +31,16 @@ async function reqAjax(url, method, params) {
 
     return getReqResults.data;
   } catch (err) {
-    return {
-      status: 444,
-      message: 'ajax Error',
-      err: err,
-      data: [],
-    };
+    console.log('ajax Error: ', err);
+    // logout
+    if (err.response.status === 401) {
+      localStorage.removeItem('user');
+      alert('로그아웃되었습니다.');
+      window.location.href = '/';
+    } else if (err.response.status === 500) {
+      alert('서버 오류');
+    }
+  } finally {
+    screen.style.display = 'none';
   }
 }
