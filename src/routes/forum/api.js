da096596867196cb278ab8a97d6f0c36abb2b9dc
Jeffrey <ref6318@gmail.com>
2021-11-14 01:09:48 +0900

    fix: list and create 추가

--- src/routes/forum/api.js
+++ src/routes/forum/api.js
@@ -1,7 +1,9 @@
 const express = require('express');
 
 const router = express.Router();
+const forumController = require('../../controller/api/forum');
 
-router.post('/list', (req, res) => {});
+router.post('/list', forumController.list);
+router.post('/write', forumController.write);
 
 module.exports = router;
