ce7ef4cd0d55a89d2e4f5c213cf8810344d845d1
Jeffrey <ref6318@gmail.com>
2021-11-15 01:24:04 +0900

    fix: cors url 수정

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -35,7 +35,7 @@ module.exports = app => {
 
     // 개발 서버
     'https://dev-file-world.loca.lt',
-    'https://file-world.herokuapp.com/',
+    'https://file-world.herokuapp.com',
   ];
 
   const corsOption = {
