b2639b89e12abeda573daa7aa1e4339acb6dc3d8
Jeffrey <ref6318@gmail.com>
2021-11-15 14:43:30 +0900

    test

--- src/models/db.js
+++ src/models/db.js
@@ -2,18 +2,18 @@ const mysql = require('mysql');
 const { local, dev, prod } = require('../config/db');
 const MakeResponse = require('../controller/handler/MakeResponse');
 
-// let connDBType = '';
+let connDBType = '';
 
-// if (process.env.NODE_ENV === 'prod') {
-//   connDBType = prod;
-// } else if (process.env.NODE_ENV === 'dev') {
-//   connDBType = dev;
-// } else {
-//   connDBType = local;
-// }
+if (process.env.NODE_ENV === 'prod') {
+  connDBType = prod;
+} else if (process.env.NODE_ENV === 'dev') {
+  connDBType = dev;
+} else {
+  connDBType = local;
+}
 
 function connDB() {
-  const conn = mysql.createConnection(dev);
+  const conn = mysql.createConnection(connDBType);
   conn.connect();
 
   return conn;
