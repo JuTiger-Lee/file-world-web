6ab79df9ae124c53e566ceb136d9b6a2319189ee
Jeffrey <ref6318@gmail.com>
2021-11-15 14:18:59 +0900

    fix: server type에 따른 db 적용

--- src/config/db.js
+++ src/config/db.js
@@ -3,17 +3,27 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 module.exports = {
-  development: {
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_DATABASE,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
+  local: {
+    user: process.env.LOCAL_DB_USER,
+    host: process.env.LOCAL_DB_HOST,
+    database: process.env.LOCAL_DB_DATABASE,
+    password: process.env.LOCAL_DB_PASSWORD,
+    port: process.env.LOCAL_DB_PORT,
     dateStrings: ['DATE', 'DATETIME'],
     timezone: '+09:00',
     multipleStatements: true,
   },
-  production: {
+  dev: {
+    user: process.env.DEV_DB_USER,
+    host: process.env.DEV_DB_HOST,
+    database: process.env.DEV_DB_DATABASE,
+    password: process.env.DEV_DB_PASSWORD,
+    port: process.env.DEV_DB_PORT,
+    dateStrings: ['DATE', 'DATETIME'],
+    timezone: '+09:00',
+    multipleStatements: true,
+  },
+  prod: {
     user: '',
     host: '',
     database: '',
