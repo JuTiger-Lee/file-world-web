0093243dd20374c5e402c2f1c2db1866d95f76d8
Jeffrey <ref6318@gmail.com>
2021-11-22 10:54:48 +0900

    fix: model 함수 이름 변경으로 인한 코드 수정

--- src/routes/forum/api.js
+++ src/routes/forum/api.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 const forumController = require('../../controller/api/forum');
 
-router.post('/list', forumController.list);
+router.get('/list', forumController.list);
 router.post('/write', forumController.write);
 
 module.exports = router;
