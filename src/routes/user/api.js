5371164b9d7c3272c782a882c7a7b849140bcd63
Jeffrey <ref6318@gmail.com>
2021-11-22 10:55:57 +0900

    fix: profileUpload 함수 추가

--- src/routes/user/api.js
+++ src/routes/user/api.js
@@ -1,12 +1,22 @@
 const express = require('express');
+const upload = require('../../middleware/multer');
 
 const router = express.Router();
 const userController = require('../../controller/api/user');
 
 router.post('/id-check', userController.idCheck);
 router.post('/nickname-check', userController.nicknameCheck);
+
 router.post('/sign-in', userController.signIn);
 router.post('/sign-up', userController.singUp);
-router.get('/sign-out', userController.signOut);
+
+router.put(
+  '/profile-upload',
+  upload.single('profile'),
+  userController.profileUpload,
+);
+router.get('/profile', userController.profile);
+
+// router.get('/sign-out', userController.signOut);
 
 module.exports = router;
