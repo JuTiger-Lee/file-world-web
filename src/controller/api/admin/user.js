7da6b6433339d6d3a3be32b3f02897264f2b1f6b
Jeffrey <ref6318@gmail.com>
2021-11-24 14:33:03 +0900

    fix: user model 함수명 변경으로인한 코드 수정

--- src/controller/api/admin/user.js
+++ src/controller/api/admin/user.js
@@ -7,7 +7,8 @@ async function list(req, res, next) {
     const listUser = await userModel.listUser([]);
 
     makeRespone.init(200, 200, 'success');
-    res.send(makeRespone.makeSuccessResponse(listUser.data));
+
+    return res.send(makeRespone.makeSuccessResponse(listUser.data));
   } catch (err) {
     console.log('admin API: user list Error:', err);
     return next(err);
