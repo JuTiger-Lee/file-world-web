534d0651361cb2f444d38374c1c9e4ffe9833564
Jeffrey <ref6318@gmail.com>
2021-11-24 14:35:18 +0900

    fix: error 핸들링

--- src/middleware/checkAuth.js
+++ src/middleware/checkAuth.js
@@ -12,6 +12,32 @@ module.exports = (req, res, next) => {
   };
 
   passport.authenticate('jwt', { session: false }, (error, user, info) => {
+    try {
+      if (!user) {
+        /**
+         * jwt expired => info message 유효기간 지날 경우
+         * {name: "TokenExpiredError", message: "jwt expired", expiredAt: "2021-11-07T07:03:04.000Z"}
+         * invalid signature => 토큰 변조
+         * {name: "JsonWebTokenError", message: "invalid signature"}
+         */
+        if (info.name === 'TokenExpiredError') {
+          makeResponse.init(419, 419, '토큰 만료');
+        } else {
+          makeResponse.init(401, 401, '유효하지 않은 토큰');
+        }
+
+        throw makeResponse.makeErrorResponse(
+          {},
+          'passport User not found Error',
+        );
+      }
+
+      req.user = decodeToken(req.headers.authorization);
+      next();
+    } catch (err) {
+      console.error(err);
+      return next();
+    }
     if (error) {
       makeResponse.init(500, 500, 'Unauthorized Error');
       throw makeResponse.makeErrorResponse(
@@ -19,25 +45,5 @@ module.exports = (req, res, next) => {
         'passport authenticate Error',
       );
     }
-
-    if (!user) {
-      /**
-       * jwt expired => info message 유효기간 지날 경우
-       * {name: "TokenExpiredError", message: "jwt expired", expiredAt: "2021-11-07T07:03:04.000Z"}
-       * invalid signature => 토큰 변조
-       * {name: "JsonWebTokenError", message: "invalid signature"}
-       */
-      if (info.name === 'TokenExpiredError') {
-        makeResponse.init(419, 419, '토큰 만료');
-      } else {
-        makeResponse.init(401, 401, '유효하지 않은 토큰');
-      }
-
-      throw makeResponse.makeErrorResponse({}, 'passport User not found Error');
-    }
-
-    req.user = decodeToken(req.headers.authorization);
-
-    next();
   })(req, res, next);
 };
