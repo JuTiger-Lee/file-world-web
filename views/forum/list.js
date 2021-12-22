3c6ae5d1291a4d02db84436d716ecfd1122011b0
Jeffey <ref6318@gmail.com>
2021-12-23 01:13:12 +0900

    fix: like un like 기능 구현

--- views/forum/list.js
+++ views/forum/list.js
@@ -10,44 +10,65 @@ const forumListSearchBtn = document.querySelector('.forum-list-search-btn');
  * @param {String} nickanme
  * @param {String} title
  * @param {String} category
+ * @param {Number} like_count
  * @param {Date} date
  * @returns
  */
-function makeForumListTemplate(idx, nickanme, title, category, date) {
+function makeForumListTemplate(
+  idx,
+  nickanme,
+  title,
+  category,
+  like_count,
+  like_status,
+  date,
+) {
   return `<div class="card">
                 <div class="card-body">
                     <div class="forum-user-profile-box">
                         <div class="forum-profile forum-user-profile">
                             <img src="/static/images/profile/blank_profile.png" alt="user profile">
                         </div>
+                        <!--
                         <div class="forum-profile-name">
                             <p>${nickanme}</p>
                         </div>
+                        --!>
                     </div>
                     <div class="forum-post-info-box">
                         <div class="forum-list-card-header-box">
                             <div class="forum-category">${category}</div>
                             <div class="forum-title-box">
-                                <a href="/forum/detail/${idx}" class="card-title forum-list-title">${title}</a>
+                                <a href="/forum/detail/${idx}" data-number=${idx} class="card-title forum-list-title">${title}</a>
                             </div>
                         </div>
                         <div class="forum-info-box">
                             <div class="forum-comment-box">
                                 <span style="color: #12a7e4;">
                                     <i class="fas fa-comment"></i>
-                                    50 Answers
+                                    0 Answers
                                 </span>
                             </div>
                             <div class="fourm-view-box">
                                 <span style="color: #2b8d6e;">
                                     <i class="fas fa-eye"></i> 
-                                    50 Views
+                                    0 Views
                                 </span>
                             </div>
                             <div class="fourm-like-box">
                                 <span style="color: #ed635e;">
-                                    <i class="far fa-heart"></i> 
-                                    10 Like
+                                  ${(function () {
+                                    let likeStatus =
+                                      '<i class="far fa-heart like" style="cursor: pointer"></i>';
+
+                                    if (like_status === 1) {
+                                      likeStatus =
+                                        '<i class="fas fa-heart un-like" style="cursor: pointer"></i>';
+                                    }
+
+                                    return likeStatus;
+                                  })()}
+                                    ${like_count} Like
                                 </span>
                             </div>
                         </div>
@@ -58,11 +79,9 @@ function makeForumListTemplate(idx, nickanme, title, category, date) {
 
                         <div class="dropdown-menu dropdown-menu-right">
                           <a class="dropdown-item" href="#">
-                            <!-- <i class="fas fa-exclamation-triangle" style="text-align:left"></i> --!>
                             <span>Report</span>
                           </a>
                           <a class="dropdown-item" href="#">
-                            <!-- <i class="far fa-bookmark"></i> --!>
                             <span>Book Mark</span>
                           </a>
                         </div>
@@ -75,9 +94,46 @@ function makeForumListTemplate(idx, nickanme, title, category, date) {
             </div>`;
 }
 
+function likeEvent() {
+  const like = document.querySelectorAll('.fourm-like-box > span > i');
+
+  /**
+   *
+   * @param {Number} index
+   * @param {String} likeType
+   */
+  const reqLikeController = async (index, likeType) => {
+    if (likeType !== 'like' && likeType !== 'un-like') {
+      throw new Error('A non-existent Like type.');
+    }
+
+    const forunmIdx = document.querySelectorAll('.forum-title-box > a');
+    const fi_idx = forunmIdx[index].getAttribute('data-number');
+    const linkData = likeType === 'like' ? '' : `?unLikeIdx=${fi_idx}`;
+    const linkEndPoint = `${likeType}${linkData}`;
+    const likeURI = `/api/forum/${linkEndPoint}`;
+    const method = likeType === 'like' ? 'post' : 'delete';
+    const bodyData = {
+      fi_idx,
+    };
+
+    const reqResult = await reqAjax(likeURI, method, bodyData);
+
+    if (reqResult.code === 200) {
+      reqForumList(location.search.split('?')[1]);
+    }
+  };
+
+  for (let i = 0; i < like.length; i++) {
+    const likeType = like[i].classList[like[i].classList.length - 1];
+
+    like[i].addEventListener('click', () => reqLikeController(i, likeType));
+  }
+}
+
 /**
  *
- * @param {Array} reqResult
+ * @param {Object} reqResult
  * @returns
  */
 function reqDataCheck(reqResult) {
@@ -106,23 +162,22 @@ function reqDataCheck(reqResult) {
   if (reqResult.code === 200) {
     const { pagination } = reqResult.data[0];
 
-    if (reqResult.data[0].pagination.list.length) {
-      for (let i = 0; i < pagination.list.length; i++) {
-        forumListCardBox.innerHTML += makeForumListTemplate(
-          pagination.list[i].fi_idx,
-          pagination.list[i].ui_nickname,
-          pagination.list[i].fi_title,
-          pagination.list[i].fi_category,
-          pagination.list[i].update_datetime,
-        );
+    for (let i = 0; i < pagination.list.length; i++) {
+      forumListCardBox.innerHTML += makeForumListTemplate(
+        pagination.list[i].fi_idx,
+        pagination.list[i].ui_nickname,
+        pagination.list[i].fi_title,
+        pagination.list[i].fi_category,
+        pagination.list[i].like_count,
+        pagination.list[i].like_status,
+        pagination.list[i].update_datetime,
+      );
 
-        loadProfile(pagination.list[i].ui_profile_hash, i);
-      }
-
-      makePagination(pagination, reqForumList);
-    } else {
-      makePagination(pagination);
+      likeEvent();
+      loadProfile(pagination.list[i].ui_profile_hash, i);
     }
+
+    makePagination(pagination, reqForumList);
   } else {
     forumListCardBox.innerHTML = ``;
 
