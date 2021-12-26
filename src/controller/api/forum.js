543bf0de7d9fe8802f9334b18f6f232affc61fec
Jeffey <ref6318@gmail.com>
2021-12-27 00:28:05 +0900

    fix: detail api sql 수정 및 코드 가독성 높임

--- src/controller/api/forum.js
+++ src/controller/api/forum.js
@@ -16,19 +16,23 @@ async function list(req, res, next) {
   const sql = {
     list:
       'SELECT us.ui_nickname, us.ui_profile, us.ui_profile_hash,' +
-      'fo.fi_idx, fo.fi_title, fo.fi_category, fo.update_datetime, ' +
+      'fo.fi_idx, fo.fi_title, fo.fi_category, fi_view, fo.update_datetime,' +
       '(' +
-      'SELECT COUNT(*) FROM post_like AS pl ' +
-      'WHERE pl.fi_idx = fo.fi_idx' +
+      'SELECT COUNT(*) FROM forum_like AS fl ' +
+      'WHERE fl.fi_idx = fo.fi_idx' +
       ') AS like_count,' +
       '(' +
-      'SELECT IF(COUNT(*) = 1, "true", "false") FROM post_like AS pl ' +
-      'WHERE pl.fi_idx = fo.fi_idx AND pl.ui_idx = ?' +
+      'SELECT IF(COUNT(*) = 1, "true", "false") FROM forum_like AS fl ' +
+      'WHERE fl.fi_idx = fo.fi_idx AND fl.ui_idx = ?' +
       ') AS like_status,' +
+      '(' +
+      'SELECT COUNT(*) FROM forum_comment AS fc ' +
+      'WHERE fc.fi_idx = fo.fi_idx' +
+      ') AS comment_count,' +
       '(' +
       'SELECT IF(COUNT(*) >= 1, "true", "false") FROM forum AS fo ' +
       'WHERE us.ui_idx = fo.ui_idx AND fo.ui_idx = ?' +
-      ') AS post_status ' +
+      ') AS forum_status ' +
       'FROM forum AS fo INNER JOIN user AS us ON us.ui_idx = fo.ui_idx ',
     total:
       'SELECT COUNT(fo.fi_idx) AS total FROM forum AS fo ' +
@@ -66,6 +70,45 @@ async function list(req, res, next) {
   }
 }
 
+async function detail(req, res, next) {
+  const { idx } = req.params;
+  const makeResponse = new MakeResponse();
+  const auth = req.headers.authorization;
+
+  let ui_idx = decodeToken(auth);
+
+  if (!ui_idx) ui_idx = 0;
+  else ui_idx = ui_idx.idx;
+
+  try {
+    const detailForum = await forumModel.detailForum([
+      idx,
+      idx,
+      ui_idx,
+      idx,
+      ui_idx,
+      idx,
+      idx,
+    ]);
+
+    if (!detailForum.data[0].length || detailForum.data[0].length > 1) {
+      makeResponse.init(500, 500, 'detail Error');
+      throw makeResponse.makeErrorResponse({}, 'forum detail Error');
+    }
+
+    const [forum, comment] = detailForum.data;
+    const [detailPost] = forum;
+    detailPost.comments = comment;
+
+    makeResponse.init(200, 200, 'success');
+
+    return res.json(makeResponse.makeSuccessResponse([detailPost]));
+  } catch (err) {
+    console.log(err);
+    return next(err);
+  }
+}
+
 async function write(req, res, next) {
   const { fi_title, fi_category, fi_content } = req.body;
   const makeResponse = new MakeResponse();
@@ -92,6 +135,8 @@ async function write(req, res, next) {
   }
 }
 
+async function deleteForum(req, res, next) {}
+
 async function update(req, res, next) {
   try {
   } catch (err) {
@@ -100,47 +145,36 @@ async function update(req, res, next) {
   }
 }
 
-async function commentWrite(req, res, next) {
-  const { ci_contents, fi_ni_idx, ci_type } = req.body;
-
-  try {
-  } catch (err) {
-    console.log(err);
-    return next(err);
-  }
-}
-
-async function commentUpdate(req, res, next) {
-  try {
-  } catch (err) {
-    console.log(err);
-    return next(err);
-  }
-}
-
-async function detail(req, res, next) {
-  const { idx } = req.params;
+async function writeComment(req, res, next) {
+  const { fi_idx, fc_comment_idx, fc_contents } = req.body;
   const makeResponse = new MakeResponse();
 
   try {
-    const detailForum = await forumModel.detailForum([idx]);
+    const saveComment = await forumModel.createComment([
+      req.user.idx,
+      fi_idx,
+      fc_comment_idx,
+      fc_contents,
+    ]);
 
-    if (!detailForum.data.length || detailForum.data.length > 1) {
-      makeResponse.init(500, 500, 'detail Error');
-      throw makeResponse.makeErrorResponse({}, 'forum detail Error');
+    if (!saveComment.data.affectedRows) {
+      makeResponse.init(500, 500, 'comment write Error');
+      throw makeResponse.makeErrorResponse({}, 'comment insert Error');
     }
 
-    makeResponse.init(200, 200, 'success');
+    makeResponse.init(201, 200, 'success');
 
-    return res.json(makeResponse.makeSuccessResponse(detailForum.data));
+    return res.json(makeResponse.makeSuccessResponse([]));
   } catch (err) {
     console.log(err);
     return next(err);
   }
 }
 
-async function postView(req, res, next) {
-  const { fi_ci_idx, li_type } = req.body;
+async function deleteComment() {}
+
+async function updateComment(req, res, next) {
+  const makeResponse = new MakeResponse();
 
   try {
   } catch (err) {
@@ -157,7 +191,7 @@ async function like(req, res, next) {
     const checkLike = await forumModel.checkLike([req.user.idx, fi_idx]);
 
     if (checkLike.data[0].like_count) {
-      makeResponse.init(500, 500, 'likes are already enabled.');
+      makeResponse.init(500, 500, 'like are already enabled.');
       throw makeResponse.makeErrorResponse({}, 'like check Error');
     }
 
@@ -178,11 +212,11 @@ async function like(req, res, next) {
 }
 
 async function unLike(req, res, next) {
-  const { unLikeIdx } = req.query;
+  const { idx } = req.params;
   const makeResponse = new MakeResponse();
 
   try {
-    const deleteLike = await forumModel.deleteLike([req.user.idx, unLikeIdx]);
+    const deleteLike = await forumModel.deleteLike([req.user.idx, idx]);
 
     if (!deleteLike.data.affectedRows) {
       makeResponse.init(500, 500, 'unlike Error');
@@ -202,10 +236,9 @@ module.exports = {
   list,
   write,
   update,
-  commentWrite,
-  commentUpdate,
+  writeComment,
+  updateComment,
   detail,
-  postView,
   like,
   unLike,
 };
