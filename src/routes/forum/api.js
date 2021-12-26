c8a370897e73002fe8e4ffb32b81694cae90f198
Jeffey <ref6318@gmail.com>
2021-12-27 00:28:41 +0900

    fix: uri 수정

--- src/routes/forum/api.js
+++ src/routes/forum/api.js
@@ -6,9 +6,8 @@ const forumController = require('../../controller/api/forum');
 router.get('/list', forumController.list);
 router.get('/detail/:idx', forumController.detail);
 router.post('/write', forumController.write);
-router.post('/comment-write', forumController.commentWrite);
-router.post('/post-view', forumController.postView);
+router.post('/comment/write', forumController.writeComment);
 router.post('/like', forumController.like);
-router.delete('/un-like', forumController.unLike);
+router.delete('/un-like/:idx', forumController.unLike);
 
 module.exports = router;
