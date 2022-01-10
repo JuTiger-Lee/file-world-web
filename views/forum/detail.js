76cfd0e68a9e8a8a1f4774b2f306603da0d1bf21
Jeffey <ref6318@gmail.com>
2022-01-10 16:27:55 +0900

    fix: commet template 수정 및 child comment replay 기능 추가

--- views/forum/detail.js
+++ views/forum/detail.js
@@ -58,6 +58,12 @@ function getDetailTemplate(
               if (forum_status === 'true') {
                 dropdownType =
                   '<a class="dropdown-item" href="#">' +
+                  '<span>Report</span>' +
+                  '</a>' +
+                  '<a class="dropdown-item" href="#">' +
+                  '<span>Book Mark</span>' +
+                  '</a>' +
+                  '<a class="dropdown-item" href="#">' +
                   '<span>Delete</span>' +
                   '</a>';
               }
@@ -123,16 +129,25 @@ function getDetailTemplate(
         <div class="comments">
           <div class="comment-box"></div>
             <div class="comment-input-box">
-              <div id="comment-write-input"></div>
-              <button 
-                class="
-                  btn btn-primary 
-                  mt-2 
-                  forum-comment-save-btn 
-                  float-right
-                ">
-                Save
-              </button>
+              <div class="form-group">
+                <textarea 
+                  class="form-control comment-contents" 
+                  rows="10"
+                  placeholder="write comments"
+                ></textarea>
+              </div>
+              <div class="forum-detail-btn-box float-right">
+                <a href="/forum/list">
+                  <button class="btn btn-danger">Cancle</button>
+                </a>
+                <button 
+                  class="
+                    btn btn-primary 
+                    forum-comment-save-btn 
+                  ">
+                  Save
+                </button>
+              </div>
             </div>
         </div>
       </div>`;
@@ -143,26 +158,26 @@ function getDetailTemplate(
  * @param {Array} comments
  * @returns
  */
-function getCommentTemplate(comments) {
+function getCommentTemplate({
+  ui_idx,
+  fc_idx,
+  ui_profile_hash,
+  ui_nickname,
+  childComments,
+  update_datetime,
+  fc_contents,
+}) {
   return `
     ${(function () {
       let commentTemplate = '';
 
-      for (let i = 0; i < comments.length; i++) {
-        const {
-          ui_idx,
-          fc_idx,
-          ui_profile_hash,
-          ui_nickname,
-          childComments,
-          update_datetime,
-          fc_contents,
-        } = comments[i];
+      const fc_group_idx = fc_idx;
 
-        commentTemplate += `
+      commentTemplate += `
           <div 
             class="d-flex flex-row mb-2 comment-contents-box" 
             id="comment-${fc_idx}" 
+            data-group-id="${fc_group_idx}"
             data-comment-id="${fc_idx}" 
             data-user-id="${ui_idx}"
           >
@@ -171,92 +186,142 @@ function getCommentTemplate(comments) {
                 <img src="${ui_profile_hash}" width="40" class="rounded-image" />
               </div>
             </div>
-              <div class="d-flex flex-column ml-2 mb-3">
-                  <span class="name">${ui_nickname}</span>
-                  <small class="ellipsis">${update_datetime}</small>
+            <div class="d-flex flex-column ml-2 mb-3">
+              <span class="name">${ui_nickname}</span>
+              <small class="ellipsis">${update_datetime}</small>
       
-                  <div class="comment-text mt-2">
-                    ${fc_contents}
-                  </div>
-                  <div class="d-flex flex-row mt-1 mb-3 align-items-center status">
-                    <small class="comment-like" style="cursor: pointer">Like</small>
-                    <small class="comment-replay" style="cursor: pointer">Reply</small>
-                  </div>
-                  <div class="child-comment-input-box comment-input-off" style="display:none">
-                    <div class="child-comment-input"></div>
-                    <button 
-                      class="
-                        btn btn-primary 
-                        mt-2 
-                        forum-comment-save-btn 
-                        float-right"
-                    >
-                    Save
-                  </button>
-                </div>
+              <div class="comment-text mt-2">${fc_contents}</div>
+              <div class="d-flex flex-row mt-1 mb-3 align-items-center status">
+                <small class="comment-like" style="cursor: pointer">Like</small>
+                <small class="comment-replay" style="cursor: pointer">Reply</small>
+              </div>
+              <div class="child-comment-input-box comment-input-off" style="display:none">
+                <textarea 
+                  class="form-control child-comment-contents" 
+                  rows="10"
+                  placeholder="write child comments"
+                ></textarea>
+                <button 
+                  class="
+                    btn btn-primary 
+                    mt-2 
+                    forum-comment-save-btn 
+                    float-right"
+                  >
+                Save
+                </button>
               </div>
             </div>
-            <div class="child-comment-box ml-5">
-              ${(function () {
-                let childCommentTemplate = '';
+          </div>
+          
+          <div class="child-comment-box ml-5">
+            ${(function () {
+              let childCommentTemplate = '';
 
-                for (let j = 0; j < childComments.length; j++) {
-                  const {
-                    ui_idx,
-                    fc_idx,
-                    ui_profile_hash,
-                    ui_nickname,
-                    update_datetime,
-                    fc_contents,
-                  } = childComments[j];
+              for (let j = 0; j < childComments.length; j++) {
+                const {
+                  ui_idx,
+                  ui_target_nickname,
+                  fc_idx,
+                  ui_profile_hash,
+                  ui_nickname,
+                  update_datetime,
+                  fc_contents,
+                } = childComments[j];
 
-                  childCommentTemplate += `
-                      <div class="child-comment-contents-box">
+                childCommentTemplate += `
+                      <div>
                         <div 
-                          class="d-flex flex-row mb-2"
-                          id="child-comment-${fc_idx}" 
-                          data-child-comment-id="${fc_idx}" 
+                          class="d-flex flex-row mb-2 child-comment-contents-box"
+                          id="comment-${fc_idx}"
+                          data-group-id="${fc_group_idx}"
+                          data-comment-id="${fc_idx}" 
                           data-user-id="${ui_idx}"
                         >
                           <div class="forum-user-profile-box">
                             <div class="forum-profile forum-user-profile">
                               <img src="${ui_profile_hash}" width="40" class="rounded-image" />
+                            </div>
                           </div>
-                        </div>
-                        <div class="d-flex flex-column ml-2 mb-3">
-                          <span class="name">${ui_nickname}</span>
-                          <small class="ellipsis">${update_datetime}</small>
+                          <div class="d-flex flex-column ml-2 mb-3">
+                            <span class="name">${ui_nickname}</span>
+                            <small class="ellipsis">${update_datetime}</small>
                     
-                          <div class="comment-contents-box mt-2">
-                            <a href="#">
-                              <span class="comment-tag" style="color: #7b7f87; font-size: 1.2rm; font-weight: 700;">
-                                @Daniel Frozer
-                              </span>
-                            </a>
+                            <div class="mt-2">
+                              <a href="#">
+                                <span class="comment-tag" style="color: #7b7f87; font-size: 1.2rm; font-weight: 700;">
+                                  @${ui_target_nickname}
+                                </span>
+                              </a>
                             <div class="comment-text mt-2">${fc_contents}</div>
                           </div>
                           <div class="d-flex flex-row mt-1 mb-3 align-items-center status">
                             <small class="comment-like" style="cursor: pointer">Like</small>
                             <small class="comment-replay" style="cursor: pointer">Reply</small>
                           </div>
-                          <div class="child-comment-input"></div>
+                          <div class="child-comment-input-box comment-input-off" style="display:none">
+                              <textarea 
+                                class="form-control child-comment-contents" 
+                                rows="10"
+                                placeholder="write child comments"
+                              ></textarea>
+                              <button 
+                                class="
+                                  btn btn-primary 
+                                  mt-2 
+                                  forum-comment-save-btn 
+                                  float-right"
+                              >
+                              Save
+                            </button>
+                          </div>
                         </div>
                       </div>`;
-                }
+              }
 
-                return childCommentTemplate;
-              })()}
-            </div>
+              return childCommentTemplate;
+            })()}
           </div>`;
-      }
 
       return commentTemplate;
     })()}`;
 }
 
+const reqCommentSave = async (
+  fc_contents,
+  fc_replay_idx,
+  fc_target_ui_idx,
+  fc_group_idx,
+) => {
+  const bodtData = {
+    fi_idx,
+    fc_replay_idx,
+    fc_target_ui_idx,
+    fc_group_idx,
+    fc_contents: fc_contents,
+  };
+
+  const reqResult = await reqAjax(`/api/forum/comment/write`, 'post', bodtData);
+
+  if (reqResult.code === 200) reqDetail(window.scrollY);
+  else alert('작성 실패');
+};
+
+function commentSave() {
+  const commentSaveBtn = document.querySelector(
+    '.forum-detail-btn-box .forum-comment-save-btn ',
+  );
+
+  commentSaveBtn.addEventListener('click', () => {
+    const commentContents = document.querySelector('.comment-contents').value;
+
+    reqCommentSave(commentContents);
+  });
+}
+
 function showChildCommentInput() {
   const commentReplay = document.querySelectorAll(
-    '.comment-contents-box .comment-replay',
+    '.comments > .comment-box .comment-replay',
   );
 
   /**
@@ -264,8 +329,6 @@ function showChildCommentInput() {
    * @param {Number} index
    */
   const showChildComment = index => {
-    const commentContentsBox =
-      commentReplay[index].parentElement.parentElement.parentElement;
     const childCommentInputBox = document.querySelectorAll(
       '.child-comment-input-box',
     );
@@ -285,59 +348,40 @@ function showChildCommentInput() {
 
     if (childCommentInputBox[index].classList[1] === 'comment-input-off') {
       showChildInputController(false);
+
+      const commentContentsBox =
+        commentReplay[index].parentElement.parentElement.parentElement;
+
+      const fc_idx = commentContentsBox.getAttribute('data-comment-id');
+      const fc_group_idx = commentContentsBox.getAttribute('data-group-id');
+      const fc_target_ui_idx = commentContentsBox.getAttribute('data-user-id');
+
+      const childCommentInputBox = document.querySelectorAll(
+        '.child-comment-input-box .forum-comment-save-btn ',
+      );
+
+      for (let i = 0; i < childCommentInputBox.length; i++) {
+        childCommentInputBox[i].addEventListener('click', () => {
+          const commentContents = document.querySelectorAll(
+            '.child-comment-contents',
+          );
+
+          reqCommentSave(
+            commentContents[i].value,
+            fc_idx,
+            fc_target_ui_idx,
+            fc_group_idx,
+          );
+        });
+      }
     } else {
       showChildInputController(true);
     }
-
-    $(`#${commentContentsBox.id} .child-comment-input`).summernote({
-      height: 150,
-      focus: true,
-      disableResizeEditor: true,
-      focus: true,
-      lang: 'ko-KR',
-      placeholder: 'Enter a Reply comment',
-    });
   };
 
   for (let i = 0; i < commentReplay.length; i++) {
     commentReplay[i].addEventListener('click', () => showChildComment(i));
   }
-}
-
-function commentSummerNoteInit() {
-  $('#comment-write-input').summernote({
-    height: 300,
-    focus: true,
-    disableResizeEditor: true,
-    focus: true,
-    lang: 'ko-KR',
-    placeholder: 'Enter a comment',
-  });
-}
-
-function reqCommentSave() {
-  const commentSaveBtn = document.querySelector('.comment-input-box > button');
-
-  const commentSave = async () => {
-    const commentContents = $('#comment-write-input').summernote('code');
-
-    const bodtData = {
-      fi_idx,
-      fc_comment_idx: null,
-      fc_contents: commentContents,
-    };
-
-    const reqResult = await reqAjax(
-      `/api/forum/comment/write`,
-      'post',
-      bodtData,
-    );
-
-    if (reqResult.code === 200) reqDetail(window.scrollY);
-    else alert('작성 실패');
-  };
-
-  commentSaveBtn.addEventListener('click', () => commentSave());
 }
 
 /**
@@ -347,10 +391,29 @@ function reqCommentSave() {
 function reqComment(comments) {
   const commentBox = document.querySelector('.comments > .comment-box');
 
-  commentBox.innerHTML = getCommentTemplate(comments);
+  for (let i = 0; i < comments.length; i++) {
+    const {
+      ui_idx,
+      fc_idx,
+      ui_profile_hash,
+      ui_nickname,
+      childComments,
+      update_datetime,
+      fc_contents,
+    } = comments[i];
 
-  commentSummerNoteInit();
-  reqCommentSave();
+    commentBox.innerHTML += getCommentTemplate({
+      ui_idx,
+      fc_idx,
+      ui_profile_hash,
+      ui_nickname,
+      childComments,
+      update_datetime,
+      fc_contents,
+    });
+  }
+
+  commentSave();
   showChildCommentInput();
 }
 
